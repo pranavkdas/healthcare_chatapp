e0750fb72689410572e30defa5321350d7aa2aa3
pranav <pranavkdas12@gmail.com>
2025-01-17 01:39:15 +0530

    done

--- second_page.py
+++ second_page.py
@@ -3,11 +3,11 @@ import streamlit as st
 from backend.connections import client_connections
 
 if st.button("Clear data in both ChromaDB and MongoDB"):
-	connection = client_connections()
-	mongodb = connection.get_mongodb_database()
+    connection = client_connections()
+    mongodb = connection.get_mongodb_database()
     chromadb = connection.get_chromadb_client()
 
     records_collection = mongodb["insurance_records"]
 
     records_collection.delete_many({})
-	chromadb.delete_collection(name="record_summaries")
+    chromadb.delete_collection(name="record_summaries")
