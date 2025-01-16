f336530a1faddc0ca7ab7729c9cf9b198ad1e2f8
pranav <pranavkdas12@gmail.com>
2025-01-17 01:45:22 +0530

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ with st.sidebar:
     )
 
 home_page = st.Page("home.py", title="Home", icon=":material/home:")
-second_page = st.Page("second_page.py", title="Settings", icon=":material/white_circle")
+second_page = st.Page("second_page.py", title="Settings", icon=":material/settings:")
 
 pg = st.navigation([home_page, second_page])
 st.set_page_config(
